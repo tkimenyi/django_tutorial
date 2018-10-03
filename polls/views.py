ec1314228fbd33f770962f94522ccb3fdd9e7a8c
Thierry Kimenyi <thierry.kimenyi@rover.com>
2018-10-03 14:58:37 -0700

    initializing the repo

---
+++ polls/views.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+from django.db.models import F
+
+
+# Create your views here.
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+#
+#     # we use the below instead of the above because doing the above couple the model
+#     # layer to the view later. One of the foremost design goals of Django is to maintain
+#     # loose coupling. Some controlled coupling is introduced in the Django.shortcuts module
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # using F avoids race conditions.
+        # read more here https://docs.djangoproject.com/en/1.11/ref/models/expressions/#avoiding-race-conditions-using-f
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # reverse() function in the HttpResponseRedirect constructor helps avoid
+        # having hard code a URL in the view function. it is given the name od
+        # view we want to pass control to and the variable portion of the url
+        # pattern that points to the view.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
